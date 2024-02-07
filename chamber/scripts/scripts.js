d1a0a28c29ac2873bbe46995b49cee1e79412cf9
BlacIP <omotoyinbobade15@gmail.com>
2024-02-07 08:52:24 +0100

    wk4

---
+++ chamber/scripts/scripts.js
@@ -0,0 +1,14 @@
+const currentYear = new Date().getFullYear();
+document.querySelector('#year').textContent = `© ${currentYear} | Lekki Chamber of Commerce | Boluwatife Adebiyi Omotoyinbo | WDD230 Project`;
+
+
+const lastModifiedDate = document.lastModified;
+document.getElementById('lastModified').textContent = `| Last Modified: ${lastModifiedDate}`;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.nav');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
