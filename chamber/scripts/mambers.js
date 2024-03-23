c6789960c8f25660fce86f20da5c290ba54cc837
BlacIP <omotoyinbobade15@gmail.com>
2024-03-23 17:36:54 +0100

    acoorect

--- chamber/scripts/mambers.js
+++ chamber/scripts/mambers.js
@@ -39,10 +39,11 @@ document.addEventListener("DOMContentLoaded", function() {
             // Populate member card with data
             memberCard.innerHTML = `
               <h2>${member.name}</h2>
+              <img src="${baseURL}${member.image}" alt="${member.name} Logo">
               <p><strong>Address:</strong> ${member.address}</p>
               <p><strong>Phone:</strong> ${member.phone}</p>
               <p><strong>Website:</strong> <a href="${member.website}" target="_blank">${member.website}</a></p>
-              <img src="${baseURL}images/${member.image}" alt="${member.name} Logo">
+              
               <p><strong>Membership Level:</strong> ${member.membership_level}</p>
               <p>${member.other_information}</p>
             `;
