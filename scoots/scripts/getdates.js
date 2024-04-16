e6c60a071e18365dd48cba70b95de63a801f3f52
BlacIP <omotoyinbobade15@gmail.com>
2024-04-16 23:10:49 +0100

    scoots

--- scoots/scripts/getdates.js
+++ scoots/scripts/getdates.js
@@ -112,3 +112,11 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.nav-menu');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
